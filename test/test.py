f72f804fccbd40818bae80f6cb10f65ac86b99b7
izabelly_zalinelo <izabellyozalinelo@gmail.com>
2025-04-21 17:28:06 -0300

    treste

--- test/test.py
+++ test/test.py
@@ -2,38 +2,41 @@ import pytest
 from unittest.mock import patch
 from src.main import *
 
-
 @pytest.mark.asyncio
-async def test_root_async():
-    result = await root()
-    assert result == {"message": "Hello World"}
-
-
-def test_funcaoteste():
+async def test_funcaoteste():
     with patch('random.randint', return_value=12345):
-        result = funcaoteste()
+        result = await funcaoteste()  # Aguardar a coroutine
         assert result == {"teste": True, "num_aleatorio": 12345}
 
 
-def test_create_estudante():
+@pytest.mark.asyncio
+async def test_create_estudante():
     estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=False)
-    result = create_estudante(estudante_teste)
+    result = await create_estudante(estudante_teste)  # Aguardar a coroutine
     assert estudante_teste == result
 
 
-def test_update_estudante_negativo():
-    assert not update_estudante(-5)
+@pytest.mark.asyncio
+async def test_update_estudante_negativo():
+    result = await update_estudante(-5)  # Aguardar a coroutine
+    assert not result
 
 
-def test_update_estudante_positivo():
-    assert update_estudante(10)
+@pytest.mark.asyncio
+async def test_delete_estudante_negativo():
+    result = await delete_estudante(-5)  # Aguardar a coroutine
+    assert not result
 
 
-def test_delete_estudante_negativo():
-    assert not delete_estudante(-5)
+@pytest.mark.asyncio
+async def test_update_estudante_positivo():
+    result = await update_estudante(10)  # Aguardar a coroutine
+    assert result
 
 
-def test_delete_estudante_positivo():
+@pytest.mark.asyncio
+async def test_delete_estudante_positivo():
     estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=True)
-    create_estudante(estudante_teste)
-    assert delete_estudante(5)
+    await create_estudante(estudante_teste)  # Aguardar a criação
+    result = await delete_estudante(5)  # Aguardar a exclusão
+    assert result
